b14eabf09304a367425f3481dab042ac0fe131d3
louistrong-debug <louistrong@gmail.com>
2026-08-16 01:57:44 +0700

    Add files via upload

--- diem_danh.py
+++ diem_danh.py
@@ -337,13 +337,12 @@ def apply_custom_css():
     """, unsafe_allow_html=True)
 
 # 🟢 DÁN ĐOẠN CODE NÀY VÀO ĐÂY:
-@st.dialog("🎉 Điểm Danh Thành Công")
+@st.dialog("🎉 Điểm Danh")
 def success_attendance_dialog(selected_name, nq_title, formatted_time):
     st.markdown(f"""
-        <div style="text-align: center; padding: 8px;">
-            <h3 style="color: #F97316 !important; margin-bottom: 9px;">Xác nhận thành công!</h3>
+        <div style="text-align: center; padding: 10px;">
+            <h3 style="color: #EA580C !important; margin-bottom: 9px;">Xác nhận thành công!</h2>
             <p style="font-size: 17px; color: #1E293B; margin-bottom: 5px;">Cảm ơn đồng chí: <b>{selected_name}</b></p>
-            <p style="font-size: 16px; color: #475569; margin-bottom: 5px;">Sự kiện: <b>{nq_title}</b></p>
             <p style="font-size: 15px; color: #64748B;">Thời gian: <b>{formatted_time}</b></p>
         </div>
     """, unsafe_allow_html=True)
